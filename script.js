4adfc5f54bb5421f3264e00d4e2d21569481bcdf
domcodes <email@domcodes.com>
2017-03-24 18:34:21 +0000

    Create 1st 2 prompts

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
 
 // 2. In this JavaScript file, add a prompt dialog to capture input from the user and store it in a variable
+var firstName = prompt('What is your first name?');
 
 // 3. Add a second a prompt dialog to capture input from the user and store it in a second variable
-
+var lastName = prompt('What is your last name?');
 // 4. Create a third variable and which combines an uppercase version values in the two other variables separated by a space. For example, if the first two variables contain "sally" and "forth", this third variable should contain the string value "SALLY FORTH"
 
 // 5. Create a fourth variable to store a number. The number should be the total number of characters in the third variable. 
